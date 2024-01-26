b26aa61c68a0fa09efdf50f831bfc92f6cb877a2
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-01-26 16:00:21 -0500

    Clean up code for Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,17 +1,13 @@
 const repeatString = function(string, n) {
     // Check for negative repetition count
     if (n < 0)
-    {
         return "ERROR";
-    }
 
     // Add n repetitions of specified string to empty string
     let repeatedString = "";
     for (let i = 0; i < n; i++)
-    {
         repeatedString += string;
-    }
-    
+
     return repeatedString;
 };
 
