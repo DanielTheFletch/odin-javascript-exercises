bc5effb72642aef8a3828b25775f7f8d730ec178
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-02-09 18:13:33 -0500

    Complete 12: Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ages = people.map(person => {
+        if (person.yearOfDeath)
+            return person.yearOfDeath - person.yearOfBirth
+        else
+        {
+            currentYear = (new Date()).getFullYear();
+            return currentYear - person.yearOfBirth;
+        }
+    });
+    const indexOldest = ages.indexOf(Math.max(...ages));
+    return people[indexOldest];
 };
 
 // Do not edit below this line
