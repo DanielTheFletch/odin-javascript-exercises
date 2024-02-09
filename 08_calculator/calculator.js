06e299767479cc30ee99bad964c63ea11b22e44e
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-02-09 16:47:33 -0500

    Complete 08: Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(addends) {
+  return addends.reduce((sum, addend) => sum + addend, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(factors) {
+  return factors.reduce((product, factor) => product * factor, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let result = 1;
+  for (let i = 0; i < exp; i++)
+    result *= base;
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  // Recursively multiply values
+  if (n > 1)
+    return n * factorial(n - 1);
+
+  // Base case (e.g., 0 or 1)
+  else
+    return 1;
 };
 
 // Do not edit below this line
