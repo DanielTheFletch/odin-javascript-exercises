bb8491ae4cbc44f579ecf059628b5b704782f0e7
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-02-09 17:26:38 -0500

    Complete 10: Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
 
+    // Check for invalid inputs
+    const n = typeof value === 'number' ? value : parseInt(value);
+    if (n === NaN || n < 0)
+        return 'OOPS';
+
+    // Evaluate Fibonacci sequence
+    let curr = 0;
+    let next = 1;
+    for (let i = 0; i < n; i++)
+    {
+        const sum = curr + next;
+        curr = next;
+        next = sum;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
